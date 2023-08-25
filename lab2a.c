a62b2e7914ac8f6872f2116d7209b3c616b3995e
unknown <galgaligowri@gmail.com>
2023-08-25 15:31:49 +0530

    C Programming

---
+++ lab2a.c
@@ -0,0 +1,33 @@
+//Linear Search
+#include<stdio.h>
+#include<time.h>
+int search(int array[],int key,int size);
+int main(){
+
+int key,a,size;
+printf("Enter the size of the array:");
+scanf("%d",&size);
+if (size <= 0) {
+        printf("Array size must be greater than 0.\n");
+        return 1; 
+    }
+int array[size];
+for(int i=0;i<size;i++){
+    array[i]=i*5;
+}
+printf("Enter the key:");
+scanf("%d",&key);
+clock_t t;
+t=clock();
+a=search(array,key,size);
+printf("%d",a);
+t=clock()-t;
+    printf("\nThe execution time is %ld",((double)t)/CLOCKS_PER_SEC);
+}
+int search(int array[],int key,int size){
+for(int i=0;i<size;i++){
+    if(key==array[i])
+    return 1;
+}
+return -1;
+}
